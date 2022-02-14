0cee610ff9ffe627f4071dd4b88173034646ac0a
Guilherme de Sá Christovão <guildschris@gmail.com>
2022-02-13 21:27:35 -0300

    :building_construction: Update ProfileViewModel to use an abstract repository

--- feature/profile/src/main/java/com/guichristovao/appstartup/profile/ui/state/ProfileViewModel.kt
+++ feature/profile/src/main/java/com/guichristovao/appstartup/profile/ui/state/ProfileViewModel.kt
@@ -3,7 +3,7 @@ package com.guichristovao.appstartup.profile.ui.state
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.guichristovao.appstartup.network_support.ExceptionHandler
-import com.guichristovao.appstartup.profile.data.source.DefaultProfileRepository
+import com.guichristovao.appstartup.profile.data.source.ProfileRepository
 import com.guichristovao.appstartup.theme.ui.component.User
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
@@ -14,7 +14,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val defaultProfileRepository: DefaultProfileRepository,
+    private val defaultProfileRepository: ProfileRepository,
     private val exceptionHandler: @JvmSuppressWildcards ExceptionHandler
 ) : ViewModel() {
 
